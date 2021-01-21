623fbb66626bbe84bbcd8c4da0364cc3334fdd45
maanijou <reza.maanijou@gmail.com>
2021-01-21 10:36:52 +0100

    Init server

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ You can use `make` to run the images.
 
 You have to define a `database.env` file in your root folder so that everything works fine. env files are usually some samples (`database.env.sample`) and should not be inside the repository. Here I just put that for your convenience!
 
+## Handling external API
+
+For external API's there are multiple scenarios and approaches. One can synchronize data between two services. In a microservice architecture, it's always good to have separate data storage and handle them separately. So rather than getting all the employees' data and saving them locally, I'm going to handle table joins and data dependencies at the app level.
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
 	log.Println("Running Project Manager app.")
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	s := http.Server{
+		Addr:         ":8080",           // configure the bind address
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
